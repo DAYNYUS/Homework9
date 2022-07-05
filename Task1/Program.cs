d15ddd33c44879daf95c86408e78d4db2223f16c
unknown <excaliburbmw@mail.ru>
2022-07-05 10:28:28 +0200

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"//
+
+int n = InputInt("Задайте число N: ");
+int count = 2;
+Console.WriteLine($"Натуральные числа от {n} до 1 равны: ");
+Console.Write(NaturalNumber(n, count));
+Console.Write(", 1");
+
+int NaturalNumber(int n, int count)
+{
+    if (count >= n)
+    {
+       return n;
+    } 
+      else
+    {
+       Console.Write($"{NaturalNumber(n, count + 1)}, ");
+    }
+
+    return count;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
